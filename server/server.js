c0a9502e8a1203dce3e7d0c3e2fac62f0ba64ff3
gaynor-landmark <g.l.landmark@gmail.com>
2016-06-11 21:21:54 +1200

    serving index.html

--- server/server.js
+++ server/server.js
@@ -2,17 +2,13 @@ var express = require('express')
 var path = require('path')
 var app = express()
 
-
-
-app.use(express.static(path.join(__dirname, 'public') ) )
+app.use(express.static(path.join(__dirname, '../public') ) )
 
 app.set('port', 3000)
 var server = app.listen(app.get('port'), function(){
   console.log('Listening on ' + server.address().port)
 })
 
-
-
 app.get('/', function(req, res){
-  res.send('hello from express')
+  res.send('FileMerge')
 })
